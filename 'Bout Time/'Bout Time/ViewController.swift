59193e53edbac80b7d26946dafface08aaa0c28a
Abhilash Muraleedharan <amuraleedharan13@gmail.com>
2018-08-14 17:20:31 +0530

    Adds Event Labels

--- 'Bout Time/'Bout Time/ViewController.swift
+++ 'Bout Time/'Bout Time/ViewController.swift
@@ -14,11 +14,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // To achieve a status bar with white content
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
+    
+    
 
 
 }
